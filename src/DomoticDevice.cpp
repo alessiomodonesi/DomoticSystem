d26083175c4b370a9441706ff2d67d00ecd8d7e5
alessiomodonesi <alessiomodo2004@gmail.com>
2025-01-08 20:28:57 +0100

    Update DomoticDevice.cpp

--- src/DomoticDevice.cpp
+++ src/DomoticDevice.cpp
@@ -1,10 +1,11 @@
 // @author Alessio Modonesi
 
+#include <iostream>
 #include "DomoticDevice.h"
 
 // Costruttore: inizializza il dispositivo con un ID, un nome e un valore di consumo energetico.
 DomoticDevice::DomoticDevice(const std::string &name, double powerConsumption)
-    : id_(std::hash<std::string>{}(name)), name_(name), powerConsumption_(powerConsumption), totalEnergyConsumption_(0) {} // AGGIUNTA
+    : id_(std::hash<std::string>{}(name)), name_(name), powerConsumption_(powerConsumption), totalEnergyConsumption_(0) {}
 
 // Metodo per accendere il dispositivo.
 void DomoticDevice::turnOn()
@@ -31,8 +32,8 @@ void DomoticDevice::setTimer(const Time &startTime, const Time &offTime)
 double DomoticDevice::calculateEnergyConsumption(const Time &startTime, const Time &offTime)
 {
     Time intervals = offTime - startTime;
-    double useTime = intervals.h + (intervals.m / 60);
-    totalEnergyConsumption_ += powerConsumption_ * useTime; // AGGIUNTA
+    double useTime = intervals.getHours() + (intervals.getMinutes() / 60);
+    totalEnergyConsumption_ += powerConsumption_ * useTime;
     return powerConsumption_ * useTime;
 }
 
@@ -42,10 +43,10 @@ DomoticDevice::~DomoticDevice() = default;
 // Ritorna lo stato del dispositivo in formato leggibile.
 std::ostream &operator<<(std::ostream &os, const DomoticDevice &device)
 {
-    os << "ID: " << getId()
-       << "\nName: " << getName()
-       << "\nStatus: " << (isDeviceOn() ? "Acceso" : "Spento")
-       << "\nEnergyConsumption: " << calculateEnergyConsumption(getStartTime(), getOffTime()) << " kW\n";
-    << "\nTotalEnergyConsumption: " << getTotalEnergyConsumption() << " kW\n";
+    os << "ID: " << device.getId()
+       << "\nName: " << device.getName()
+       << "\nStatus: " << (device.isDeviceOn() ? "Acceso" : "Spento")
+       << "\nEnergyConsumption: " << device.calculateEnergyConsumption(device.getStartTime(), device.getOffTime()) << " kW\n"
+       << "\nTotalEnergyConsumption: " << device.getTotalEnergyConsumption() << " kW\n";
     return os;
 }
